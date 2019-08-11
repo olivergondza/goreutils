ae5b2c1089b83e59ff0522534f3448f53fcb6a85
Oliver Gondža <ogondza@gmail.com>
2019-08-11 23:34:48 +0200

    Capture output

--- test/assert.sh
+++ test/assert.sh
@@ -3,25 +3,42 @@
 set -uo pipefail
 trap 's=$?; echo "$0: Error on line "$LINENO": $BASH_COMMAND"; exit $s' ERR
 
+export PATH="$BIN:$PATH"
+
 # Args
 
 ## Invalid
-[ "$(${BIN}/assert --message        2>&1)" == "Option --message given with no value" ]
-[ "$(${BIN}/assert --exit-with      2>&1)" == "Option --exit-with given with no value" ]
-[ "$(${BIN}/assert --stdout-matches 2>&1)" == "Option --stdout-matches given with no value" ]
-[ "$(${BIN}/assert --stderr-matches 2>&1)" == "Option --stderr-matches given with no value" ]
-[ "$(${BIN}/assert --running        2>&1)" == "Option --running given with no value" ]
+[ "$(assert --message        2>&1)" == "Option --message given with no value" ]
+[ "$(assert --exit-with      2>&1)" == "Option --exit-with given with no value" ]
+[ "$(assert --stdout-matches 2>&1)" == "Option --stdout-matches given with no value" ]
+[ "$(assert --stderr-matches 2>&1)" == "Option --stderr-matches given with no value" ]
+[ "$(assert --running        2>&1)" == "Option --running given with no value" ]
 
-[ "$(${BIN}/assert 2>&1)" == "No operation specified. Use --running" ]
+[ "$(assert 2>&1)" == "No operation specified. Use --running" ]
 
 # Valid
 
-${BIN}/assert --running true --succeeds --no-stderr --no-stdout
-${BIN}/assert --running true --exit-with 0 --no-stderr --no-stdout
+assert --running true --succeeds --no-stderr --no-stdout
+assert --running true --exit-with 0 --no-stderr --no-stdout
+assert --running true --succeeds-silently
 
-${BIN}/assert --exit-with 0 --no-stderr --stdout-matches "42" \
+assert --exit-with 0 --stdout-matches "42" \
     --running ansible,-a,'awk "BEGIN {print 6*7}"',localhost
 
-${BIN}/assert --succeeds --stdout-matches "42" \
+assert --succeeds --stdout-matches "42" \
     --running ansible,-a,"awk 'BEGIN {print 6*7}'",localhost
 
+# Correctness
+
+## Exit
+[ "$(assert --running true  --exit-with 1       2>&1)" == "ASSERT: Exit code mismatch. Expected 1, was 0" ]
+[ "$(assert --running false --exit-with 0       2>&1)" == "ASSERT: Exit code mismatch. Expected 0, was 1" ]
+[ "$(assert --running false --succeeds-silently 2>&1)" == "ASSERT: Exit code mismatch. Expected 0, was 1" ]
+
+## Outputs
+[ "$(assert --running seq,1 --succeeds-silently 2>&1 || true)" == $'ASSERT: Standard output mismatch. Expected text matching \'^$\', was:\n1\nASSERT: ===' ]
+[ "$(assert --running 'sh,-c,seq 1 >&2' --succeeds-silently 2>&1 || true)" == $'ASSERT: Standard error mismatch. Expected text matching \'^$\', was:\n1\nASSERT: ===' ]
+
+# Environment
+export ASSERT_TEST_VAR=42
+assert --running sh,-c,'printf $ASSERT_TEST_VAR' --succeeds --no-stderr --stdout-matches "^42$"
