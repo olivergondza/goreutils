8b1d4a3b6e8d32144fa3630403dd2b81bce0999a
Oliver Gondža <ogondza@gmail.com>
2019-08-12 22:20:55 +0200

    Port tests to use assert for testing

--- test/assert.sh
+++ test/assert.sh
@@ -3,42 +3,42 @@
 set -uo pipefail
 trap 's=$?; echo "$0: Error on line "$LINENO": $BASH_COMMAND"; exit $s' ERR
 
-export PATH="$BIN:$PATH"
+# assrt is a tool for test, assert is a system under test
+export PATH="$BIN:$BIN/../test:$PATH"
 
 # Args
 
 ## Invalid
-[ "$(assert --message        2>&1)" == "Option --message given with no value" ]
-[ "$(assert --exit-with      2>&1)" == "Option --exit-with given with no value" ]
-[ "$(assert --stdout-matches 2>&1)" == "Option --stdout-matches given with no value" ]
-[ "$(assert --stderr-matches 2>&1)" == "Option --stderr-matches given with no value" ]
-[ "$(assert --running        2>&1)" == "Option --running given with no value" ]
+assrt --running assert,--message --exit-with 1 --no-stdout --stderr-matches '^Option --message given with no value\n$'
+assrt --running assert,--exit-with --exit-with 1 --no-stdout --stderr-matches '^Option --exit-with given with no value\n$'
+assrt --running assert,--stdout-matches --exit-with 1 --no-stdout --stderr-matches '^Option --stdout-matches given with no value\n$'
+assrt --running assert,--stderr-matches --exit-with 1 --no-stdout --stderr-matches '^Option --stderr-matches given with no value\n$'
+assrt --running assert,--running --exit-with 1 --no-stdout --stderr-matches '^Option --running given with no value\n$'
 
-[ "$(assert 2>&1)" == "No operation specified. Use --running" ]
+assrt --running assert --exit-with 1 --no-stdout --stderr-matches '^No operation specified. Use --running\n$'
 
 # Valid
 
-assert --running true --succeeds --no-stderr --no-stdout
-assert --running true --exit-with 0 --no-stderr --no-stdout
-assert --running true --succeeds-silently
+assrt --succeeds-silently --running 'assert,--running,true,--succeeds,--no-stderr,--no-stdout'
+assrt --succeeds-silently --running 'assert,--running,true,--exit-with,0,--no-stderr,--no-stdout'
+assrt --succeeds-silently --running 'assert,--running,true,--succeeds-silently'
 
-assert --exit-with 0 --stdout-matches "42" \
-    --running ansible,-a,'awk "BEGIN {print 6*7}"',localhost
-
-assert --succeeds --stdout-matches "42" \
-    --running ansible,-a,"awk 'BEGIN {print 6*7}'",localhost
+assrt --succeeds-silently --running \
+    'assert,--exit-with,0,--stdout-matches,42,--running,ansible\,-a\,awk "BEGIN {print 6*7}"\,localhost'
+assrt --succeeds-silently --running \
+    "assert,--succeeds,--stdout-matches,42,--running,ansible\,-a\,awk 'BEGIN {print 6*7}'\,localhost"
 
 # Correctness
 
 ## Exit
-[ "$(assert --running true  --exit-with 1       2>&1)" == "ASSERT: Exit code mismatch. Expected 1, was 0" ]
-[ "$(assert --running false --exit-with 0       2>&1)" == "ASSERT: Exit code mismatch. Expected 0, was 1" ]
-[ "$(assert --running false --succeeds-silently 2>&1)" == "ASSERT: Exit code mismatch. Expected 0, was 1" ]
+assrt --running 'assert,--running,true,--exit-with,1' --exit-with 2 --no-stdout --stderr-matches '^ASSERT: Exit code mismatch. Expected 1, was 0\n'
+assrt --running 'assert,--running,false,--exit-with,0' --exit-with 2 --no-stdout --stderr-matches '^ASSERT: Exit code mismatch. Expected 0, was 1\n$'
+assrt --running 'assert,--running,false,--succeeds-silently' --exit-with 2 --no-stdout --stderr-matches '^ASSERT: Exit code mismatch. Expected 0, was 1\n$'
 
 ## Outputs
-[ "$(assert --running seq,1 --succeeds-silently 2>&1 || true)" == $'ASSERT: Standard output mismatch. Expected text matching \'^$\', was:\n1\nASSERT: ===' ]
-[ "$(assert --running 'sh,-c,seq 1 >&2' --succeeds-silently 2>&1 || true)" == $'ASSERT: Standard error mismatch. Expected text matching \'^$\', was:\n1\nASSERT: ===' ]
+assrt --running 'assert,--running,seq\,1,--succeeds-silently' --exit-with 2 --no-stdout --stderr-matches $'ASSERT: Standard output mismatch. Expected text matching \'\^\$\', was:\n1\nASSERT: ===\n'
+assrt --running 'assert,--running,sh\,-c\,seq 1 >&2,--succeeds-silently' --exit-with 2 --no-stdout --stderr-matches $'ASSERT: Standard error mismatch. Expected text matching \'\^\$\', was:\n1\nASSERT: ===\n'
 
 # Environment
 export ASSERT_TEST_VAR=42
-assert --running sh,-c,'printf $ASSERT_TEST_VAR' --succeeds --no-stderr --stdout-matches "^42$"
+assrt --running 'assert,--running,sh\,-c\,printf "$ASSERT_TEST_VAR",--succeeds,--no-stderr,--stdout-matches,42' --succeeds-silently
