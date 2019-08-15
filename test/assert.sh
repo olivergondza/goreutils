39be88b5fca5ff4727658681599b00507e7f0a69
Oliver Gondža <ogondza@gmail.com>
2019-08-15 22:11:37 +0200

    Support out equals and equals-file

--- test/assert.sh
+++ test/assert.sh
@@ -5,6 +5,7 @@ trap 's=$?; echo "$0: Error on line "$LINENO": $BASH_COMMAND"; exit $s' ERR
 
 # assrt is a tool for test, assert is a system under test
 export PATH="$BIN:$BIN/../test:$PATH"
+export RES="$BIN/../test/assert"
 
 # Args
 
@@ -41,7 +42,6 @@ assrt --succeeds-silently --running \
 
 # Correctness
 
-## Exit
 assrt --running 'assert,--running,true,--exit-with,1' \
     --exit-with 2 --no-stdout --stderr-matches $'^ASSERT: Failed running: \'true\'\n  - Exit code mismatch. Expected 1, was 0\n'
 
@@ -51,7 +51,8 @@ assrt --running 'assert,--running,false,--exit-with,0' \
 assrt --running 'assert,--running,false,--succeeds-silently' \
     --exit-with 2 --no-stdout --stderr-matches $'^ASSERT: Failed running: \'false\'\n  - Exit code mismatch. Expected 0, was 1\n$'
 
-## Outputs
+## Output
+### no-stdout/no-stderr
 assrt --running 'assert,--running,seq\,1,--succeeds-silently' \
     --exit-with 2 --no-stdout --stderr-matches \
     $'ASSERT: Failed running: \'seq\' \'1\'\n  - Expected stdout matching \'\^\$\', was:\n    1\n'
@@ -59,6 +60,26 @@ assrt --running 'assert,--running,seq\,1,--succeeds-silently' \
 assrt --running 'assert,--running,sh\,-c\,seq 1 >&2,--succeeds-silently' \
     --exit-with 2 --no-stdout --stderr-matches \
     $'ASSERT: Failed running: \'sh\' \'-c\' \'seq 1 >&2\'\n  - Expected stderr matching \'\^\$\', was:\n    1\n'
+
+### stdout-equals
+assrt --running 'assert,--running,printf\,foobar,--succeeds,--stdout-equals,foobar' --succeeds-silently
+assrt --running 'assert,--running,printf\,foo,--succeeds,--stdout-equals,bar' --exit-with 2 --no-stdout --stderr-matches \
+    $'ASSERT: Failed running: \'printf\' \'foo\'\n  - Expected stdout: \'bar\', was:\n    foo'
+
+### stderr-equals
+assrt --running 'assert,--running,sh\,-c\,printf foobar >&2,--no-stdout,--stderr-equals,foobar' --succeeds-silently
+assrt --running 'assert,--running,sh\,-c\,printf foobar >&2,--no-stdout,--stderr-equals,foobaz' --exit-with 2 --no-stdout \
+    --stderr-matches $'ASSERT: Failed running: \'sh\' \'-c\' \'printf foobar >&2\'\n  - Expected stderr: \'foobaz\', was:\n    foobar'
+
+### stdout-equals-file
+assrt --running "assert,--running,cat\,$RES/multiline,--succeeds,--stdout-equals-file,$RES/multiline" --succeeds-silently
+assrt --running "assert,--running,cat\,-n\,$RES/multiline,--succeeds,--stdout-equals-file,$RES/multiline" --exit-with 2 --no-stdout \
+    --stderr-matches '3\s+ever'
+
+### stderr-equals-file
+assrt --running "assert,--running,sh\,-c\,cat $RES/multiline >&2,--exit-with,0,--stderr-equals-file,$RES/multiline" --succeeds-silently
+assrt --running "assert,--running,sh\,-c\,cat -n $RES/multiline >&2,--succeeds,--stderr-equals-file,$RES/multiline" --exit-with 2 --no-stdout \
+    --stderr-matches '3\s+ever'
 
 # Environment
 export ASSERT_TEST_VAR=42
