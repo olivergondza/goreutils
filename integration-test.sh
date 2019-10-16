d308654a7aa981a6c5a9e39d20a811acacb13cb9
Oliver Gondža <ogondza@gmail.com>
2019-10-16 10:20:45 +0200

    Correctly propagate error status on test failure

--- integration-test.sh
+++ integration-test.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # Inspired by https://disconnected.systems/blog/another-bash-strict-mode/
-#set -uo pipefail
+set -ueo pipefail
 trap 's=$?; echo >&2 "$0: Error on line "$LINENO": $BASH_COMMAND"; exit $s' ERR
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
@@ -15,19 +15,28 @@ targets=(
   "ubuntu" #No out of the box support: gawk required
 )
 
+function wrap_in_container() {
+  target="$1"
+  tag="$2"
+  docker build --force-rm -t "$tag" -f "./integration-test/test-containers/${target}" ./integration-test/test-containers/
+  docker run --rm --volume "${DIR}:/tmp/goreutils" "$tag" "/tmp/goreutils/test.sh"
+}
+
+total=0
 function run_test() {
   target="$1"
   tag="goreutils-it-${target}"
-  docker build --force-rm -t "$tag" -f "./integration-test/test-containers/${target}" ./integration-test/test-containers/
-  docker run --rm --volume "${DIR}:/tmp/goreutils" "$tag" "/tmp/goreutils/test.sh"
-  if [ "$?" == 0 ]; then
+  if wrap_in_container "$target" "$tag" 2>&1 | sed 's/^/    /'; then
     echo "SUCCESS"
   else
     echo "FAILURE"
+    total=1
   fi
 }
 
 for target in "${targets[@]}"; do
   echo "=== $target ==="
-  run_test "$target" 2>&1 | sed 's/^/    /'
+  run_test "$target"
 done
+
+exit "$total"
--- test.sh
+++ test.sh
@@ -1,13 +1,20 @@
 #!/usr/bin/env bash
 # https://disconnected.systems/blog/another-bash-strict-mode/
-set -uo pipefail
+set -euo pipefail
 trap 's=$?; echo "$0: Error on line "$LINENO": $BASH_COMMAND"; exit $s' ERR
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
+status=0
 export BIN="$DIR"/bin
 for test in ${DIR}/test/*.sh; do
   echo "$test"
   cd "$DIR"
-  "$test"
+
+  # Print everything to stderr and fail if there is 'ASSERT:'
+  if ! "$test" 2>&1 | awk "BEFORE {error=0}; //; /ASSERT:/ { error=1 }; END {exit error}" >&2; then
+    status=1
+  fi
 done
+
+exit "$status"
